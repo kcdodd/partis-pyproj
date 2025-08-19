5a5b84703ea02e6fe3510d930a0ca06f56fc5b2f
Carter Dodd <cdodd@nanohmics.com>
2025-08-19 12:17:40 -0500

    Ensure dist exists before install deps

--- noxfile.py
+++ noxfile.py
@@ -37,6 +37,7 @@ dist_dir = work_dir/'dist'
 build_dir = work_dir/'build'
 reports_dir = work_dir/'reports'
 
+dist_dir.mkdir(exist_ok = True)
 os.environ['PIP_FIND_LINKS'] = str(dist_dir)
 os.environ['UV_FIND_LINKS'] = str(dist_dir)
 
